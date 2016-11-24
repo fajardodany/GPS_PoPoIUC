721076d55c8c2fff5b0b6c2312592261cae2c41d
Carolina <carolina96brasio@gmail.com>
2016-11-24 16:22:28 +0000

    first draft

---
+++ app/src/main/java/pt/isec/a21240085/popoiuc/InserirDadosActivity.java
@@ -0,0 +1,13 @@
+package pt.isec.a21240085.popoiuc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InserirDadosActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inserir_dados);
+    }
+}
