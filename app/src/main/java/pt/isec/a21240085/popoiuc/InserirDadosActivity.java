052603bfe97540cbe0aa76cc518777685340cb85
Carolina <carolina96brasio@gmail.com>
2016-11-24 18:19:23 +0000

    vertical layouts

--- app/src/main/java/pt/isec/a21240085/popoiuc/InserirDadosActivity.java
+++ app/src/main/java/pt/isec/a21240085/popoiuc/InserirDadosActivity.java
@@ -2,6 +2,7 @@ package pt.isec.a21240085.popoiuc;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class InserirDadosActivity extends Activity {
 
@@ -9,5 +10,11 @@ public class InserirDadosActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inserir_dados);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_inserirdados, menu);
+        return true;
     }
 }
