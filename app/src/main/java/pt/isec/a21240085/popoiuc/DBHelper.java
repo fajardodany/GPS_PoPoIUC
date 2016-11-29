8a7e1e20f96e7c3acbefdace3bbde55a3a55c81e
Francisco Pronto <fpronto94@gmail.com>
2016-11-29 17:20:54 +0000

    valores xml

--- app/src/main/java/pt/isec/a21240085/popoiuc/DBHelper.java
+++ app/src/main/java/pt/isec/a21240085/popoiuc/DBHelper.java
@@ -1,8 +1,29 @@
 package pt.isec.a21240085.popoiuc;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by francisco on 28-11-2016.
  */
 
-public class DBHelper {
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "popoIUC.db";
+    public static final String CONTACTS_TABLE_NAME = "contacts";
+
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
--- app/src/main/java/pt/isec/a21240085/popoiuc/MainActivity.java
+++ app/src/main/java/pt/isec/a21240085/popoiuc/MainActivity.java
@@ -2,12 +2,22 @@ package pt.isec.a21240085.popoiuc;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    Button comeco;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        comeco = (Button) findViewById(R.id.buttonComecar);
+
+        openOrCreateDatabase("",MODE_PRIVATE,null);
+    }
+
+    protected void clicar(){
+
     }
 }
